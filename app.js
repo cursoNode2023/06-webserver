e5ed94d6eeba377fc2a4aa105abce6de04634100
fdurante <fdurante@alu.ucam.edu>
2023-01-02 17:42:11 +0100

    Lección 92

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 8080;
+const hbs = require('hbs');
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Middleware : Es lo primero que se ejecuta ( contenido estático )
 app.use( express.static('public') );
 
-
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola mundo en su respectiva ruta');
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Francisco Durante',
+        titulo: 'Curso de Node'
+    });
 });
 
 app.get('/generic', (req, res) => {
-    res.sendFile( __dirname + '/public/generic.html');
+    res.sendFile( __dirname + '/public/generic.htm')
 });
 
 app.get('/elements', (req, res) => {
-    res.sendFile( __dirname + '/public/elements.html');
+    res.sendFile( __dirname + '/public/elements.html')
 });
 
 app.get('*', (req, res) => {
